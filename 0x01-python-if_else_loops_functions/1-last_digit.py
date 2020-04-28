d4b7a694f1ed45d2baf999e9ba3bcbc532d8117c
nickarshadi <1477@holbertonschool.com>
2020-04-28 21:05:09 +0000

    Remove negative condition

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -5,10 +5,10 @@ x = -10
 if number >= 0:
     x = 10
 lastnumber = number % x
-if lastnumber > 5 or lastnumber < -5:
-    detail = "and is greater than 5"
+if lastnumber > 5:
+    detail = "greater than 5"
 elif lastnumber == 0:
-    detail = "and is 0"
+    detail = "0"
 else:
-    detail = "and is less than 6 and not 0"
-print("Last digit of {:d} is {:d} {}".format(number, lastnumber, detail))
+    detail = "less than 6 and not 0"
+print("Last digit of {:d} is {:d} and is {}".format(number, lastnumber, detail))
