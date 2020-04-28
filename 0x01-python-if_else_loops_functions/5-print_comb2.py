7f63ceb66ddd255875e853f702b933471b58291f
nickarshadi <1477@holbertonschool.com>
2020-04-28 22:23:51 +0000

    Reset

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for i in range(0, 100):
-    print("{:d}{:d}, ".format(i // 10, i % 10), end='')
-print("{:d}".format(99))
+for i in range(100):
+    print("{:02d}, ".format(i), end='')
+print("{:02d}".format(99))
