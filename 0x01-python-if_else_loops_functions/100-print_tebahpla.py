d4f7c7c29a1fd478e1d8fc72350d3cb79f208aaf
nickarshadi <1477@holbertonschool.com>
2020-04-29 05:06:17 +0000

    Correct

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 for i in range(90, 64, -1):
     if i % 2 == 0:
+        j = 32
     else:
-    print(chr(i+322), end='')
+        j = 0
+    print("{}".format(chr(i+j)), end='')
