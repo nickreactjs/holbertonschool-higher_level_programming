200e77baea3e02f6101462d38bb781963dc763d1
nickarshadi <1477@holbertonschool.com>
2020-05-03 04:47:11 +0000

    Fix indentation

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -3,8 +3,8 @@ if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
     from sys import argv, exit
     if len(argv) != 4:
-       print("Usage: ./100-my_calculator.py <a> <operator> <b>") 
-       exit(1)
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
     a = int(argv[1])
     operator = argv[2]
     b = int(argv[3])
