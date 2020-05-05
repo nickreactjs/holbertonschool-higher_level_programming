cc3bf52b036108a1e4e23fab27dcb9de6d3d3efb
nickarshadi <1477@holbertonschool.com>
2020-05-05 22:32:24 +0000

    Remove uppercase

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    my_string.upper()
     strlen = len(my_string)
     i = 0
     while i < strlen:
-        if my_string[i] == 'C':
+        if my_string[i] == 'C' or my_string[i] == 'c':
             my_string = my_string[:i] + my_string[i + 1:]
             strlen -= 1
         i += 1
