75a246bea3e260276cc1de4bd21902433265cfff
nickarshadi <1477@holbertonschool.com>
2020-05-05 22:59:23 +0000

    Print matrix

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for ti in matrix:
+        i = 0
+        if isinstance(ti, list):
+            for dti in ti:
+                i += 1
+                print("{:d}".format(dti), end='')
+                if i != len(ti):
+                    print(" ", end='')
+            i = 0
+            print()
+        else:
+            print(ti)
