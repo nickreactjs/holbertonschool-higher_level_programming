b1e718ab6f9e2a9e57a23be3836914bab02a47e5
nickarshadi <1477@holbertonschool.com>
2020-05-05 21:40:22 +0000

    If index greater of equal

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    if len(my_list) == 0:
-        return (None)
-    elif idx < 0 or idx > len(my_list):
+    elif idx < 0 or idx >= len(my_list):
         return (None)
     return (my_list[idx])
