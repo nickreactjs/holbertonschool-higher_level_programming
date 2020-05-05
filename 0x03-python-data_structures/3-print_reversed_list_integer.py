33d34b06020ca3ac87e11ce771debb1c9f95e814
nickarshadi <1477@holbertonschool.com>
2020-05-05 22:08:22 +0000

    Return none if NULL

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    copy = my_list.copy()
-    copy.reverse()
-    for tidentifier in copy:
+    if not my_list:
+        return None
+    my_list.reverse()
+    for tidentifier in my_list:
         print("{:d}".format(tidentifier))
