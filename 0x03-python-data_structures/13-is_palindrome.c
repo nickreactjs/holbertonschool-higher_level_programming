7cf8562c1ba5d20a584676a180fa1ce4b0daa580
nickarshadi <1477@holbertonschool.com>
2020-05-06 04:26:56 +0000

    Check for [alindrome

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+/**
+  * is_palindrome - check if linked list is a palindrome
+  * empty list is considered a palindrome
+  * head: head of linked list
+  * Return: 0 if it is not a palindrome and 1 if it is
+  */
+int is_palindrome(listint_t **head)
+{
+
+}
