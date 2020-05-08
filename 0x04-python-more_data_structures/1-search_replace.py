d987089982e44dd5465c548bbf3b141b27aa958b
nickarshadi <1477@holbertonschool.com>
2020-05-08 23:41:12 +0000

    Replace elements in list

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
+
+
 def search_replace(my_list, search, replace):
-    return list(map(lambda e: replace if e == search else e, my_list))
+    newlist = my_list.copy()
+    for i in range(len(newlist)):
+        if newlist[i] == search:
+            newlist[i] = replace
+
+    return newlist
