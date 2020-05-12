2318c0c85cf50c53296ca84783739a6013b7fe2f
nickarshadi <1477@holbertonschool.com>
2020-05-12 12:31:42 +0000

    Add romans

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -5,16 +5,16 @@ def roman_to_int(roman_string):
     rint = 0
     if roman_string and isinstance(roman_string, str):
         for ti in roman_string:
-            if ti == 'X':
-                rint += 10
+            if ti == 'I':
+                rint += 1
             elif ti == 'V':
                 rint += 5
-            elif ti == 'I':
-                rint += 1
+            elif ti == 'X':
+                rint += 10
             elif ti == "L":
                 rint += 50
-            elif ti == 'D':
-                rint += 500
             elif ti == 'C':
                 rint += 100
+            elif ti == 'D':
+                rint += 500
     return rint
