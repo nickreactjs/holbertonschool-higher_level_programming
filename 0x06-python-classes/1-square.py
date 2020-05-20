d487990744b4b93cd3eb07fdf4e9f01d44bbc517
nickarshadi <1477@holbertonschool.com>
2020-05-20 22:00:40 +0000

    Set square with privater fiueld size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Square module"""
+
+
+class Square:
+    """class square"""
+
+    def __init__(self, size):
+        """init class with size
+
+        Args:
+            size: size of square
+        """
+        self.__size = size
