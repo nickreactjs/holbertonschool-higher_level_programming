43867f78f4a077027b18e49aae88af2c6147df9a
nickarshadi <1477@holbertonschool.com>
2020-05-22 23:15:52 +0000

    Test edge cases for integer addition

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -3,14 +3,25 @@
 
 
 def add_integer(a, b=98):
-    """Add Integer"""
+    """Add Integer
+
+    ArgsL
+        a: first int
+        b: second int
+
+    Raises:
+        TypeError: if a, b are not int or float.
+
+    Returns:
+        Sum of both integers.
+    """
 
     if not (isinstance(a, float) or isinstance(a, int)):
         raise TypeError("a must be an integer")
     if not (isinstance(b, float) or isinstance(b, int)):
         raise TypeError("b must be an integer")
-    if isinstance(a, float):
-        a = int(a)
-    if isinstance(b, float):
-        b = int(b)
-    return a + b
+    return int(a) + int(b)
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile("tests/0-add_integer.txt")
