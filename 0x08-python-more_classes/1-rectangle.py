4d6f70d0317109aa33294233386ba630e0c73b50
nickarshadi <1477@holbertonschool.com>
2020-05-26 03:29:12 +0000

    Make attribute public

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -12,8 +12,8 @@ class Rectangle:
             width: rectangle width
             height: rectangle height
         """
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
