87046ba87cdec88091df63c0ec8424cfabe70ccd
nickarshadi <1477@holbertonschool.com>
2020-06-17 23:28:07 -0500

    Remove C

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    strlen = len(my_string)
     i = 0
-    while i < strlen:
-        if my_string[i] == 'C' or my_string[i] == 'c':
+    for char in my_string:
+        if (char == 'c' or char == 'C'):
             my_string = my_string[:i] + my_string[i + 1:]
-            strlen -= 1
+            i -= 1
         i += 1
-    return (my_string)
+    return my_string
