f0a4b86909e0670a7f24b08c6a0f82e3204898fc
nickarshadi <1477@holbertonschool.com>
2020-06-17 23:39:54 -0500

    Fix lenght

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,6 +3,6 @@ def print_matrix_integer(matrix=[[]]):
     for ti in matrix:
         for dti in ti:
             print("{:d}".format(dti), end='')
-            if ti.index(dti) != len(ti):
+            if ti.index(dti) != len(ti) - 1:
                 print(" ", end='')
         print()
