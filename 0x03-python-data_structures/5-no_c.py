41b7188027f45d0606ddf0726dd364b1401facbb
nickarshadi <1477@holbertonschool.com>
2020-06-17 23:37:50 -0500

    Print integer matrix

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
+    if not my_string:
+        return None
     i = 0
     for char in my_string:
         if (char == 'c' or char == 'C'):
--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,16 +1,8 @@
 #!/usr/bin/python3
-
-
 def print_matrix_integer(matrix=[[]]):
     for ti in matrix:
-        i = 0
-        if isinstance(ti, list):
-            for dti in ti:
-                i += 1
-                print("{:d}".format(dti), end='')
-                if i != len(ti):
-                    print(" ", end='')
-            i = 0
-            print()
-        else:
-            print(ti)
+        for dti in ti:
+            print("{:d}".format(dti), end='')
+            if ti.index(dti) != len(ti):
+                print(" ", end='')
+        print()
