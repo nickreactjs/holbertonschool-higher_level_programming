bb429df4f4d0e504f2b0050d0b9b991d1a2d8ba8
nickarshadi <1477@holbertonschool.com>
2020-06-25 21:49:24 -0500

    Check best score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,11 +2,11 @@
 
 
 def best_score(a_dictionary):
-    kmax = None
-    vmax = 0
-    if isinstance(a_dictionary, dict):
-        for k, v in a_dictionary.items():
-            if v > vmax:
-                vmax = v
-                kmax = k
-    return kmax
+    max = 0
+    if not a_dictionary:
+        return None
+    for k, v in a_dictionary.items():
+        if v > max:
+            max = v
+            winner = k
+    return winner
