a4d715dd2d013e71c8b1be4f51da8194712258ac
nickarshadi <1477@holbertonschool.com>
2020-06-25 21:42:31 -0500

    Create dict comprehension

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -2,7 +2,4 @@
 
 
 def multiply_by_2(a_dictionary):
-    ndict = a_dictionary.copy()
-    for k, v in ndict.items():
-        ndict[k] = v * 2
-    return ndict
+    return {k: v * 2 for k, v in a_dictionary.items()}
