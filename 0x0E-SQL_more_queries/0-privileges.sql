e9616c013e693c72564746483587fc570998631d
nickarshadi <1477@holbertonschool.com>
2020-07-17 20:35:55 -0500

    SHOW GRANT

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,5 +1,3 @@
--- My privileges
--- show privs for user1
-SHOW GRANTS FOR user_0d_1@localhost;
--- show privs for user2
-SHOW GRANTS FOR user_0d_2@localhost;
+-- Show all privileg from users
+SHOW GRANTS FOR 'user_0d_1';
+SHOW GRANTS FOR 'user_0d_2';
