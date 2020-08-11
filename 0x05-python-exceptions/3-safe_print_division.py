cbe7b03711e7fb6f15d73e7f95624afd1658b69d
nickarshadi <1477@holbertonschool.com>
2020-08-11 17:42:02 -0500

    ZeroDivisionError

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
-
-
 def safe_print_division(a, b):
     try:
-        a = a / b
+        c = a / b
     except ZeroDivisionError:
-        a = None
+        c = None
     finally:
-        print("Inside result:", "{}".format(a))
-    return a
+        print("Inside result: {}".format(c))
+        return c
