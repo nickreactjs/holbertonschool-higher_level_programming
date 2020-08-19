f53cfad92b81d95e011b463bd6b5f7f694a28bc9
nickarshadi <1477@holbertonschool.com>
2020-08-19 18:12:45 -0500

    Shorten lon line

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -22,7 +22,8 @@ def matrix_divided(matrix, div):
         if len(i) != lenr:
             check2 = 1
     if check == 1:
-        raise TypeError('matrix must be a matrix (list of lists) of integers/floats')
+        raise TypeError('matrix must be a matrix (list of lists)\
+        of integers/floats')
     if check2 == 1:
         raise TypeError('Each row of the matrix must have the same size')
     if not isinstance(div, int) and not isinstance(div, float):
@@ -30,6 +31,7 @@ def matrix_divided(matrix, div):
     if div == 0:
         raise ZeroDivisionError('division by zero')
     return [[round(j / div, 2) for j in i] for i in matrix]
+
 
 if __name__ == '__main__':
     import doctest
