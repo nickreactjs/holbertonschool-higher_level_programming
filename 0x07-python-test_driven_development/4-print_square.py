bb3db19aef0250cfb094c19c1620fce6cbb911f9
nickarshadi <1477@holbertonschool.com>
2020-08-19 20:28:16 -0500

    Check

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -14,12 +14,13 @@ def print_square(size):
         raise ValueError("size must be >= 0")
     if size > 0:
         row = ''
-        row = '#'* size
+        row = '#' * size
         row = row + '\n'
         print(row * size)
     else:
         print()
 
-    if __name__ == '__main__':
-        import doctest
-        doctest.testfile("tests/4-print_square.txt")
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testfile("tests/4-print_square.txt")
