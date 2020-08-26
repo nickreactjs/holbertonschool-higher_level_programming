f1c5908ff827d76ab2c8c32ed3df4d37b1ca1ce8
nickarshadi <1477@holbertonschool.com>
2020-08-26 10:19:34 -0500

    Remove last newline

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -55,4 +55,4 @@ class Rectangle:
         if self.width == 0 or self.height == 0:
             return ''
         w = '#' * self.width + '\n'
-        return w * self.height
+        return (w * self.height)[:-1]
