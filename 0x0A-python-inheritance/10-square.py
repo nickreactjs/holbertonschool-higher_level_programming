aaaa52ddccbd970a467fabdde85f9e1cb8527d2a
nickarshadi <1477@holbertonschool.com>
2020-08-27 17:52:16 -0500

    super().__init__(arg1, arg2) in init

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
-'''Module for Rectangle class.'''
+""" Module for Square."""
 Rectangle = __import__('9-rectangle').Rectangle
 
 
+
 class Square(Rectangle):
-    """A subclass representing a rectangle."""
+    """ Square class. """
+
     def __init__(self, size):
-        """Constructor"""
+        """ Init"""
         self.integer_validator("size", size)
         self.__size = size
         super().__init__(size, size)
 
     def area(self):
-        """Method for area of square."""
         return self.__size ** 2
