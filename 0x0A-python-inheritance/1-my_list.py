9ba70c2377043f4286ee5a845356d1cb6049cae6
nickarshadi <1477@holbertonschool.com>
2020-08-26 21:02:54 -0500

    type 0bj

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-'''Module for MyList class.'''
+"""Module My list."""
 
 
 class MyList(list):
-    '''MyList class.'''
+    """Class that inherits from list."""
+
     def print_sorted(self):
-        '''Print sorted list.'''
+        """Print the list in sorted order."""
         print(sorted(self))
--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
-"""Module for is_same_class method."""
+"""Module is same class"""
 
 
 def is_same_class(obj, a_class):
-    """Determine if an object is exactly an instance of a class."""
-    return type(obj) == a_class
-
+    """Check if obj is same class."""
+    if isinstance(obj, a_class):
+        if type(obj) == a_class:
+            return True
+        else:
+            return False
