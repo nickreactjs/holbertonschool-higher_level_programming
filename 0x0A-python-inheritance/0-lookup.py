ed376c1bb3fe9a8aa896fa2a0f913e164df1c6de
nickarshadi <1477@holbertonschool.com>
2020-08-26 19:20:39 -0500

    dir()

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,13 +1,3 @@
-#!/usr/bin/python3
-'''Lookup method module.'''
-
-
 def lookup(obj):
-    '''Look up object attributes and methods.
-    Args:
-        obj: the object to list.
-
-    Returns:
-        list: the list of attributes.
-    '''
+    """ Print all attributes and methods. """
     return dir(obj)
