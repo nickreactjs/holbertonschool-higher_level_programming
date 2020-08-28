9debe66210ae06dc8bc4d325e7b7aeb9e474d7c8
nickarshadi <1477@holbertonschool.com>
2020-08-28 17:44:27 -0500

    add complete file path

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 import json
 import sys
-import os
+import os.path
 save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
 
