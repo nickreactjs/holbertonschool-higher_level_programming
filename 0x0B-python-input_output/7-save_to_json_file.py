d5585d7d592f4bd7e79bec99c68d1750ac4ca9cb
nickarshadi <1477@holbertonschool.com>
2020-08-28 17:17:16 -0500

    json.dump(obj, f)

--- 0x0B-python-input_output/7-save_to_json_file.py
+++ 0x0B-python-input_output/7-save_to_json_file.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-'''Module for save_to_json_file method.'''
+"""json.dump Module. """
 import json
 
 
 def save_to_json_file(my_obj, filename):
-    '''Method for saving json file.'''
-    with open(filename, "w", encoding="utf-8") as f:
+    """ Write an Object to a text file, using a JSON
+        representation. """
+    with open(filename, 'w', encoding='utf-8') as f:
         json.dump(my_obj, f)
