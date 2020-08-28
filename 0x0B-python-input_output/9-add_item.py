5a51ae30001c413224a83a04811d6d0fbf571b42
nickarshadi <1477@holbertonschool.com>
2020-08-28 17:46:44 -0500

    Remove Argv[0] filename

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -10,6 +10,6 @@ file = 'add_item.json'
 json_list = []
 if os.path.exists(file):
     json_list = load_from_json_file(file)
-for ar in sys.argv:
-    json_list.append(ar)
+for i in range(1, len(sys.argv)):
+    json_list.append(sys.argv[i])
 save_to_json_file(json_list, file)
