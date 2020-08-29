99a0f55e46def411b6e106141a96fc8811626d75
nickarshadi <1477@holbertonschool.com>
2020-08-28 19:22:07 -0500

    self.__dict__[k] = v

--- 0x0B-python-input_output/13-student.py
+++ 0x0B-python-input_output/13-student.py
@@ -32,4 +32,4 @@ class Student:
     def reload_from_json(self, json):
         """ Replace all attributes of the Student instance. """
         for k, v in json.items():
-            self.k = v
+            self.__dict__[k] = v
