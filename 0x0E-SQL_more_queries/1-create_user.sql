d65dc6b0ff6a42f7209dc53e1d3286bacd82c1b2
nickarshadi <1477@holbertonschool.com>
2020-11-13 16:20:40 -0500

    Create a new User.'

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,5 @@
+-- Create the MySQL server user user_0d_1
+-- User has all privileges, password should be set to user_0d_1_pwd
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON * . * TO 'user_0d_1'@'localhost';
+FLUSH PRIVILEGES;
