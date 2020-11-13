433177b209356b730743278d646f0793ad97c31a
nickarshadi <1477@holbertonschool.com>
2020-11-13 15:51:59 -0500

    Check USER grants

--- 0x0E-SQL_more_queries/5-unique_id.sql
+++
@@ -1,6 +0,0 @@
--- Unique ID
--- create table
-CREATE TABLE IF NOT EXISTS unique_id (
-    id INT DEFAULT 1 UNIQUE,
-    name VARCHAR(256)
-) ENGINE=INNODB;
--- 0x0E-SQL_more_queries/6-states.sql
+++
@@ -1,8 +0,0 @@
--- States table
--- create db
-CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
--- create table
-CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (
-    id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
-    name VARCHAR(256) NOT NULL
-) ENGINE=INNODB;
