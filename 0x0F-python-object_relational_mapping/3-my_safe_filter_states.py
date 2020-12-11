3197b8235d2532a9cc102e2b616d4dcfc1b263ed
nickarshadi <1477@holbertonschool.com>
2020-12-11 15:38:26 -0500

    Connecting mysqlserver and pythoncodebase via mysqldb.

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host='localhost', port=3306, user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
-    cur.execute("SELECT * FROM states ORDER BY states.id ASC")
+    cur.execute("SELECT * FROM states WHERE name = %s ORDER BY states.id ASC", (sys.argv[4], ))
     rows = cur.fetchall()
     for row in rows:
         print(row)
