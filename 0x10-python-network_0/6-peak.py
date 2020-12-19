e0baaf4c5dd0485fb62a3eba5d6dfe0fd620eadc
nickarshadi <1477@holbertonschool.com>
2020-12-19 13:33:36 -0500

    Replace tabs with spaces.

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """Find Peak in integerlist."""
 
+
 def find_peak(list_of_integers):
-	if not list_of_integers:
-		return None
-	return max(list_of_integers)
+    if not list_of_integers:
+        return None
+    return max(list_of_integers)
