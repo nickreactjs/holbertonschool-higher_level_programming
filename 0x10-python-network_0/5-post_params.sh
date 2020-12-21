8fe920eafa995d8a7574e22c9f97a2b0d2d6e4a6
nickarshadi <1477@holbertonschool.com>
2020-12-20 22:47:37 -0500

    Remove whitespace.

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Send a DELETE request.
-curl -s -X POST -d "email=hr@holbertonschool.com&subject= I will always be here for PLD" "$1"
+curl -s -X POST -d "email=hr@holbertonschool.com&subject=I will always be here for PLD" "$1"
