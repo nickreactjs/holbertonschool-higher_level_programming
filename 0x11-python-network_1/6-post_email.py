b402d731fc8172c6853bb95bca8fd807c6fb425d
nickarshadi <1477@holbertonschool.com>
2021-01-11 17:58:40 -0500

    Add tasks.

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Use requests module."""
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    response = requests.post(argv[1], data={'email': argv[2]})
+    print(response.text)
