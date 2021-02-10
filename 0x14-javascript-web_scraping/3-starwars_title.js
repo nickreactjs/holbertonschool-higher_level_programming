016d055d264e83ff64d3473871c741653d60e770
nickarshadi <1477@holbertonschool.com>
2021-02-10 04:32:37 +0000

    Modify starwars.

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,14 +1,10 @@
 #!/usr/bin/node
 const request = require('request');
-let url = 'http://swapi.co/api/films/';
-let episode = process.argv[2];
-request(url + episode, function (err, response, body) {
-  if (err) {
-    console.log(err);
-  } else if (response.statusCode === 200) {
-    let jsonobj = JSON.parse(body);
-    console.log(jsonobj.title);
-  } else {
-    console.log('An error occured. Status code: ' + response.statusCode);
-  }
+const id = process.argv[2];
+const url = 'https://swapi-api.hbtn.io/api/films/' + id;
+request.get(url, function (error, response, body) {
+  error = 0;
+  response = '';
+  const data = JSON.parse(body);
+  console.log(data.title);
 });
