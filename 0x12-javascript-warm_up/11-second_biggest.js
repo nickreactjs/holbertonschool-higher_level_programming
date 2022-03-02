3a7e4fa0455d8760e42bfc3a0cabafb167909b71
nickarshadi <1477@holbertonschool.com>
2022-03-02 20:10:55 +0100

    Add list.

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,2 +1,29 @@
 #!/usr/bin/node
-console.log(process.argv.length < 4 ? 0 : process.argv.slice(2).sort((a, b) => b - a)[1]);
+/* Search the second biggest integer. */
+const list = process.argv.slice(2);
+let max = list[0];
+let n, maxIndex;
+let i = 0;
+if (list.length === 0 || list.length === 1) {
+  console.log('0');
+} else {
+  while (i < list.length) {
+    n = parseInt(list[i]);
+    if (n < max) {
+      max = n;
+      maxIndex = i;
+    }
+    i += 1;
+  }
+  list.splice(maxIndex, 1);
+  i = 0;
+  max = list[0];
+  while (i < list.length) {
+    n = parseInt(list[i]);
+    if (n < max) {
+      max = n;
+    }
+    i += 1;
+  }
+  console.log(max);
+}
