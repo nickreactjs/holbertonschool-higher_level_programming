e2ec456984c87b5cfb9060ba3fd719de2044700e
nickarshadi <1477@holbertonschool.com>
2022-05-13 15:15:52 +0000

    Reverse list

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,4 +1,10 @@
 #!/usr/bin/node
 exports.esrever = function (list) {
-  return list.sort(() => 1);
+  const reversed = [];
+  let i = list.length - 1;
+  while (i >= 0) {
+    reversed.push(list[i]);
+    i -= 1;
+  }
+  return reversed;
 };
