072db9f529adf6764e0f697ec122e12301fa21ab
nickarshadi <1477@holbertonschool.com>
2022-05-14 19:48:57 +0000

    Add fs.readfile.

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,5 +1,10 @@
 #!/usr/bin/node
 const fs = require('fs');
-fs.readFile(process.argv[2], 'utf-8', (error, data) => {
-  console.log(error || data);
+const filename = process.argv[2];
+fs.readFile(filename, 'utf-8', function (err, data) {
+  if (err) {
+    console.err(err);
+    return;
+  }
+  console.log(data);
 });
