317c6ef19a031cd401e5641d65c56680470d74ee
nickarshadi <1477@holbertonschool.com>
2022-05-14 20:18:23 +0000

    Write text to file with fs.writefile.

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,6 +1,7 @@
 #!/usr/bin/node
-// reads file passed as arg
 const fs = require('fs');
-fs.writeFile(process.argv[2], process.argv[3], (error) => {
-  error && console.log(error);
+fs.writeFile(process.argv[2], process.argv[3], 'utf-8', function (err, data) {
+  if (err) {
+    console.log(err);
+  }
 });
